7c77601528a3ce5902a83818a1862500684c3f22
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-04-21 15:37:14 +1000

    Started coding assignment 6 for Sci410, I need to learn how to error handle in Matlab before finishing

---
+++ MDSC/Sci410/MachNumber.m
@@ -0,0 +1,11 @@
+
+
+while validspeedOfCraft > 0:
+    speedOfCraft = input('Please provide the speed of the aircraft: ')
+    if speedOfCraft 
+
+speedOfSound = input('Please provide the speed of sound at the aircraft\'s current altitude: ')
+machn = speedOfCraft/speedOfSound
+
+if machn > 1:
+    print('The ')
