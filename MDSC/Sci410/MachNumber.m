fb200ad7bededbcfaaa0d89d2e6ed90db010b20c
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-04-22 07:49:56 +1000

    Touched up the code, added error handling in the form of a while loop to check inputs isnumeric and > 0

--- MDSC/Sci410/MachNumber.m
+++ MDSC/Sci410/MachNumber.m
@@ -1,11 +1,23 @@
 
 
-while validspeedOfCraft > 0:
+while true
     speedOfCraft = input('Please provide the speed of the aircraft: ')
-    if speedOfCraft 
+    speedOfSound = input('Please provide the speed of sound at the aircraft\'s current altitude: ')
 
-speedOfSound = input('Please provide the speed of sound at the aircraft\'s current altitude: ')
-machn = speedOfCraft/speedOfSound
+    if isnumeric(speedOfCraft) && isnumeric(speedOfSound) && speedOfCraft > 0 && speedOfSound > 0
+    machn = speedOfCraft/speedOfSound
 
-if machn > 1:
-    print('The ')
+        if machn < 1
+            print('The flight is subsonic!')
+        elseif machn == 0
+            print('The flight is transonic!')
+        else
+            print('The flight is supersonic!')
+        end
+    break;
+    else
+        print('Please input valid integers for speed of craft and speed of sound!')
+    end  
+end  
+    
+            
