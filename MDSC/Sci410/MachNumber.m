2a64b72d606edecef7b6d0f94d9231aaa16918fe
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-04-22 13:40:01 +1000

    Made some syntax adjustments

--- MDSC/Sci410/MachNumber.m
+++ MDSC/Sci410/MachNumber.m
@@ -1,22 +1,21 @@
 
-
 while true
-    speedOfCraft = input('Please provide the speed of the aircraft: ')
-    speedOfSound = input('Please provide the speed of sound at the aircraft\'s current altitude: ')
+    speedOfCraft = input("Please provide the speed of the aircraft: ");
+    speedOfSound = input("Please provide the speed of sound at the aircraft's current altitude: ");
 
     if isnumeric(speedOfCraft) && isnumeric(speedOfSound) && speedOfCraft > 0 && speedOfSound > 0
-    machn = speedOfCraft/speedOfSound
+       machn = speedOfCraft/speedOfSound;
 
         if machn < 1
-            print('The flight is subsonic!')
+            disp("The flight is subsonic!")
         elseif machn == 0
-            print('The flight is transonic!')
+            disp("The flight is transonic!")
         else
-            print('The flight is supersonic!')
+            disp("The flight is supersonic!")
         end
     break;
     else
-        print('Please input valid integers for speed of craft and speed of sound!')
+        disp("Please input valid integers for speed of craft and speed of sound!")
     end  
 end  
     
