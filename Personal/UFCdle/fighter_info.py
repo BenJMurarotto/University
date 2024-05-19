186489ed66fd78a77d94cc8f491da0968871631f
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-05-19 22:15:10 +1000

    question answer for COSC210 theroy assignment

--- Personal/UFCdle/fighter_info.py
+++ Personal/UFCdle/fighter_info.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import random
 
 fighter_data = {}
+style_options = ['Jiu-Jitsu', 'Wrestler', 'Kickboxer', 'Sambo', 'MMA', 'Boxing', 'Freestyle']
 
 response = requests.get(f'https://www.ufc.com/{get_link()}')
 if response.status_code == 200:
@@ -12,11 +13,16 @@ if response.status_code == 200:
         scrape_division = soup.find_all("div", class_="hero-profile__division")
         hero_profile = scrape_division[0].text.split("\n")
         division = hero_profile[1].split(' ')
-        if len(division) > 2:
-                division.pop(len(division))
-                
-                        
+        for x, item in enumerate(division):
+                if item == 'Division':
+                        del division[x]         
+        dprocessing = ' '.join(map(str, division))    
+        fighter_data['Division'] = dprocessing.strip("'[],") 
 
+        scrape_style = soup.find("div", class_="c-bio__label").next_siblings
+        print(scrape_style)
+        
 
-print(fighter_data)
+        
+
         
