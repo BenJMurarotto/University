cc3590d152e32d3d8e2b644a85df9ff75367af78
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-05-25 10:58:45 +1000

    added a few assignment pieces and accessory files

--- Personal/UFCdle/fighter_info.py
+++ Personal/UFCdle/fighter_info.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import random
 
 fighter_data = {}
-style_options = ['Jiu-Jitsu', 'Wrestler', 'Kickboxer', 'Sambo', 'MMA', 'Boxing', 'Freestyle']
+style_options = ['Jiu-Jitsu', 'Wrestler', 'Kickboxer', 'Sambo', 'MMA', 'Boxing', 'Freestyle', 'Grappler']
 
 response = requests.get(f'https://www.ufc.com/{get_link()}')
 if response.status_code == 200:
@@ -19,8 +19,19 @@ if response.status_code == 200:
         dprocessing = ' '.join(map(str, division))    
         fighter_data['Division'] = dprocessing.strip("'[],") 
 
-        scrape_style = soup.find("div", class_="c-bio__label").next_siblings
+        scrape_style = soup.find_all("div", class_="c-bio__text")
+        for style in scrape_style:
+                if style.text in style_options:
+                        fighter_data['Fighting Style'] = style
+                else: 
+                        fighter_data['Fighting Style'] = 'MMA' 
+        
+        
         print(scrape_style)
+        print(len(scrape_style))
+        print(fighter_data)
+
+        
         
 
         
