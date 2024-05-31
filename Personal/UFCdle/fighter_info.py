062af7ecf3a588c28310c5aba094efa2261eb3f6
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-05-31 15:01:05 +1000

    Updated UFCdle script to include hometown

--- Personal/UFCdle/fighter_info.py
+++ Personal/UFCdle/fighter_info.py
@@ -1,15 +1,19 @@
 from main import get_link
 import requests
 from bs4 import BeautifulSoup
-import random
+import csv
 
 fighter_data = {}
 style_options = ['Jiu-Jitsu', 'Wrestler', 'Kickboxer', 'Sambo', 'MMA', 'Boxing', 'Freestyle', 'Grappler']
+csv_file = 'fighter_data.csv'
 
-response = requests.get(f'https://www.ufc.com/{get_link()}')
-if response.status_code == 200:
+def store_fighter_data():
+    response = requests.get(f'https://www.ufc.com/{get_link()}')
+    if response.status_code == 200:
         # Parse the UFC fighter content
         soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Scrape Division
         scrape_division = soup.find_all("div", class_="hero-profile__division")
         hero_profile = scrape_division[0].text.split("\n")
         division = hero_profile[1].split(' ')
@@ -19,21 +23,32 @@ if response.status_code == 200:
         dprocessing = ' '.join(map(str, division))    
         fighter_data['Division'] = dprocessing.strip("'[],") 
 
+        # Scrape Fighting Style
         scrape_style = soup.find_all("div", class_="c-bio__text")
         for style in scrape_style:
-                if style.text in style_options:
-                        fighter_data['Fighting Style'] = style
-                else: 
-                        fighter_data['Fighting Style'] = 'MMA' 
-        
-        
-        print(scrape_style)
-        print(len(scrape_style))
-        print(fighter_data)
+            if style.text.strip() in style_options:
+                fighter_data['Fighting Style'] = style.text.strip()
+                break
+        else:
+            fighter_data['Fighting Style'] = 'MMA'
+        print("Fighting Style:", fighter_data['Fighting Style'])  # Debugging statement
 
-        
-        
+        # Scrape Country
+        if len(scrape_style) > 1:
+            home_town = scrape_style[1].text.strip()
+            if ',' in home_town:
+                fighter_data['Country'] = home_town.split(', ')[-1]
+            else:
+                fighter_data['Country'] = home_town
+        else:
+            fighter_data['Country'] = 'Unknown'
+        print("Country:", fighter_data['Country'])  # Debugging statement
 
-        
+def write_to_csv(fighter_dict):
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['Division', 'Fighting Style', 'Country'])
+        writer.writeheader()
+        writer.writerow(fighter_dict)  # Write a single row
 
-        
+store_fighter_data()
+write_to_csv(fighter_data)
