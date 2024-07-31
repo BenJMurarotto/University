6a0ce163998032dfb535f793cee9864283c6e4a2
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-07-31 20:42:39 +1000

    tweaked assignment

--- MDSC/COSC210/databases/practicalassignment2.sql
+++ MDSC/COSC210/databases/practicalassignment2.sql
@@ -5,11 +5,11 @@ CREATE VIEW movie_summary AS
     ;
 
 CREATE VIEW old_shipments AS
-    SELECT c.first_name, c.last_name, m.movie_id, sh.shipment_id, sh.shipment_date
+    SELECT c.first_name, c.last_name, s.movie_id, sh.shipment_id, sh.shipment_date
     FROM customers AS c
     INNER JOIN shipments AS sh ON c.customer_id = sh.customer_id
     INNER JOIN stock AS s ON sh.movie_id = s.movie_id
-    WHERE YEAR(shipment_date) < 2010
+    WHERE EXTRACT(YEAR FROM sh.shipment_date) < 2010
     ;
 
 CREATE VIEW richie AS
@@ -19,31 +19,47 @@ CREATE VIEW richie AS
     ;
 
 CREATE VIEW retail_price_hike AS
-    SELECT movie_id, retail_price, retail_price * 1.25
+    SELECT movie_id, retail_price, retail_price * 1.25 AS price_hike
     FROM stock
     ;
 
 CREATE VIEW profits_from_movie AS
-    SELECT m.movie_id, m.movie_title, SUM(s.cost_price) - SUM(s.retail_price) AS price_diff  
+    SELECT m.movie_id, m.movie_title, SUM(s.retail_price) - SUM(s.cost_price) AS price_diff  
     FROM movies AS m
     INNER JOIN stock AS s ON s.movie_id = m.movie_id
-    GROUP BY m.movie_title
+    GROUP BY m.movie_id
+    ORDER BY price_diff ASC
     ;
 
 CREATE VIEW binge_watcher AS
-    SELECT m.first_name, m.last_name
-    FROM movies AS m
-    INNER JOIN shipments AS sh ON c.customer_id = sh.shipment_id
-    GROUP BY sh.shipment_date
+    SELECT sh.shipment_date, c.first_name, c.last_name
+    FROM shipments AS sh
+    INNER JOIN customers AS c ON c.customer_id = sh.customer_id
+    GROUP BY sh.shipment_date, c.first_name, c.last_name
     HAVING COUNT(sh.shipment_id) > 1
-
     ;
+
+
 CREATE VIEW the_sith AS
-    SELECT c.first_name, c.last_name
-    FROM customers AS c
-    INNER JOIN shipments AS sh ON sh.customer_id = c.customer_id
-    INNER JOIN stock AS s ON sh.movie_id = s.movie_id
-    INNER JOIN movies AS m ON m.movie_id = s.movie_id
-    WHERE movie_title  
-    GROUP BY customer_id
-    HAVING shipment_id
+SELECT c.first_name, c.last_name
+FROM customers AS c
+WHERE c.customer_id NOT IN (
+    SELECT DISTINCT c.customer_id
+    FROM customers as c
+    INNER JOIN shipments sh ON c.customer_id = sh.shipment_id
+    INNER JOIN stock s ON sh.media_type = s.media_type
+    INNER JOIN movies m ON s.movie_id = m.movie_id
+    WHERE movie_title = 'Star Wars: Episode V - The Empire Strikes Back'
+);
+
+SELECT c.first_name, c.last_name
+FROM customers AS c
+(
+    SELECT DISTINCT c.customer_id
+    FROM customers as c
+    INNER JOIN shipments sh ON c.customer_id = sh.shipment_id
+    INNER JOIN stock s ON sh.media_type = s.media_type
+    INNER JOIN movies m ON s.movie_id = m.movie_id
+    WHERE movie_title = '12 Angry Men'
+);
+
