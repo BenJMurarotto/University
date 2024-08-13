0fc9f1f74fe26cb61a66805e831f701ce53a25df
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-08-13 23:03:04 +1000

    added AJAX search functionality to the query of database, I will admit to anyone reading this... i dont know html or jscript very well so alot of this code was written by ai

--- Personal/UFCdle/back/app/routes.py
+++ Personal/UFCdle/back/app/routes.py
@@ -1,6 +1,26 @@
 from app import app
+from flask import request, jsonify, render_template
+import sqlite3
+
+def connect_db():
+    return sqlite3.connect('app/misc/ufcdle.db')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')
+    con = connect_db()
+    cur = con.cursor()
+
+    cur.execute(
+        '''SELECT fname, lname FROM fighters WHERE fname LIKE ? OR lname LIKE ?''', 
+        (query + '%', query + '%')
+    )
+    results = cur.fetchall()
+
+    con.close()
+    return jsonify(results)
