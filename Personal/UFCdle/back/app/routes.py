facc11772251288c8293e5f16dc6961a739eda74
BenJMurarotto <ben.j.murarotto@alumni.uts.edu.au>
2024-08-14 21:56:41 +1000

    minor html changes

--- Personal/UFCdle/back/app/routes.py
+++ Personal/UFCdle/back/app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, session
 import sqlite3
+
+app.secret_key = 'your_secret_key_here'
 
 def connect_db():
     return sqlite3.connect('app/misc/ufcdle.db')
@@ -8,6 +10,19 @@ def connect_db():
 @app.route('/')
 @app.route('/index')
 def index():
+    session.clear()  # Clear session to start fresh
+    session['guessed_fighters'] = []
+    session['max_guesses'] = 6
+
+    # Select a random secret fighter
+    con = connect_db()
+    cur = con.cursor()
+    cur.execute('SELECT fname, lname FROM fighters ORDER BY RANDOM() LIMIT 1')
+    secret_fighter = cur.fetchone()
+    con.close()
+
+    session['secret_fighter'] = {'fname': secret_fighter[0], 'lname': secret_fighter[1]}
+
     return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
@@ -16,11 +31,17 @@ def search():
     con = connect_db()
     cur = con.cursor()
 
+    # Ensure the query is case-insensitive and correctly filters fighters
     cur.execute(
-        '''SELECT fname, lname FROM fighters WHERE fname LIKE ? OR lname LIKE ?''', 
-        (query + '%', query + '%')
+        '''SELECT fname, lname FROM fighters WHERE LOWER(fname) LIKE LOWER(?) OR LOWER(lname) LIKE LOWER(?)''', 
+        (query.lower() + '%', query.lower() + '%')
     )
     results = cur.fetchall()
 
     con.close()
-    return jsonify(results)
+    
+    if results:
+        # Return an array of matching fighters
+        return jsonify([{'fname': result[0], 'lname': result[1]} for result in results])
+    else:
+        return jsonify({'status': 'error', 'message': 'No matching fighter found.'})
